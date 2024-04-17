ce44754cfc1e364eee952f4c782f4141a235be3d
zongz <zongzhe1024@163.com>
2024-04-18 00:17:45 +0800

    test

--- setup-nexus.sh
+++ setup-nexus.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Start Nexus OSS
-docker run -d -p 8081:8081 --name nexus sonatype/nexus3
+# Create a network
+docker network create mynetwork
+
+# Start Nexus OSS in the created network
+docker run -d -p 8081:8081 --network=mynetwork --name nexus sonatype/nexus3
 
 # Configure Nexus Repository
 echo "Waiting for Nexus to start..."
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8081)" != "200" ]]; do sleep 5; done
-curl -u admin:admin123 --header "Content-Type: application/json" 'http://localhost:8081/service/rest/v1/security/users' -d'
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://nexus:8081)" != "200" ]]; do sleep 5; done
+curl -u admin:admin123 --header "Content-Type: application/json" 'http://nexus:8081/service/rest/v1/security/users' -d'
 {
   "userId": "test",
   "firstName": "test",
