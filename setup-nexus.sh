5cfb4fca1c15727055c5effd45a5b0e3f88361fa
zongz <289202471@qq.com>
2024-04-17 14:21:56 +0800

    fix: add ci to test
    Signed-off-by: zongz <289202471@qq.com>

---
+++ setup-nexus.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Start Nexus OSS
+docker run -d -p 8081:8081 --name nexus sonatype/nexus3
+
+# Configure Nexus Repository
+echo "Waiting for Nexus to start..."
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8081)" != "200" ]]; do sleep 5; done
+curl -u admin:admin123 --header "Content-Type: application/json" 'http://localhost:8081/service/rest/v1/security/users' -d'
+{
+  "userId": "test",
+  "firstName": "test",
+  "lastName": "test",
+  "emailAddress": "test@example.com",
+  "password": "1234",
+  "status": "active",
+  "roles": ["nx-admin"]
+}'
